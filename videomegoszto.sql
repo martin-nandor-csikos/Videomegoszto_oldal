ff03f47926bc00b00ec3501b50c0ce2cc5ba4a88
Martin Nándor Csíkos <74834714+MateLUL@users.noreply.github.com>
2024-03-22 10:42:00 +0100

    Mysql --> Oracle (ON UPDATE meg nincs)

--- videomegoszto.sql
+++ videomegoszto.sql
@@ -1,230 +1,253 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: mysql
--- Generation Time: Mar 20, 2024 at 02:50 PM
--- Server version: 8.3.0
--- PHP Version: 8.2.17
+DROP TABLE video_cimke;
+DROP TABLE video_kategoria;
+DROP TABLE eredet;
+DROP TABLE feltolto;
+DROP TABLE iro;
+DROP TABLE kedvenc;
+DROP TABLE felhasznalo;
+DROP TABLE kategoria;
+DROP TABLE komment;
+DROP TABLE video;
+DROP TABLE cimke;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+
+CREATE TABLE cimke (
+  id NUMBER(10) NOT NULL,
+  cim VARCHAR2(255) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT cim UNIQUE (cim)
+);
+
+COMMENT ON TABLE cimke IS 'A videók lehetséges címkéit tartalmazó tábla.';
+
+CREATE SEQUENCE cimke_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER cimke_seq_tr
+BEFORE INSERT ON cimke
+FOR EACH ROW
+WHEN (NEW.id IS NULL)
+BEGIN
+  SELECT cimke_seq.NEXTVAL INTO :NEW.id FROM DUAL;
+END;
+/
+
+COMMENT ON COLUMN cimke.id IS 'A címke azonosítója (kulcs)';
+COMMENT ON COLUMN cimke.cim IS 'A címke címe (egyedi)';
+
+CREATE TABLE eredet (
+  komment_id NUMBER(10) NOT NULL,
+  video_id NUMBER(10) NOT NULL,
+  PRIMARY KEY (komment_id, video_id)
+);
+
+COMMENT ON TABLE eredet IS 'A kommentek eredet videóját jelölő tábla.';
+COMMENT ON COLUMN eredet.komment_id IS 'A komment azonosítója (kulcs)';
+COMMENT ON COLUMN eredet.video_id IS 'A videó azonosítója (kulcs)';
+
+CREATE INDEX komment_id ON eredet (komment_id);
+CREATE INDEX video_id ON eredet (video_id);
+
+CREATE TABLE felhasznalo (
+  id NUMBER(10) NOT NULL,
+  nev VARCHAR2(30) NOT NULL,
+  email VARCHAR2(50) NOT NULL,
+  jelszo VARCHAR2(255) NOT NULL,
+  admin NUMBER(3) DEFAULT 0 NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT email_jelszo UNIQUE (email, jelszo)
+);
+
+COMMENT ON TABLE felhasznalo IS 'A felhasználók adatait tartalmazó tábla.';
+
+CREATE SEQUENCE felhasznalo_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER felhasznalo_seq_tr
+BEFORE INSERT ON felhasznalo
+FOR EACH ROW
+WHEN (NEW.id IS NULL)
+BEGIN
+  SELECT felhasznalo_seq.NEXTVAL INTO :NEW.id FROM DUAL;
+END;
+/
+
+COMMENT ON COLUMN felhasznalo.id IS 'A felhasználó azonosítója (kulcs)';
+COMMENT ON COLUMN felhasznalo.nev IS 'A felhasználónév';
+COMMENT ON COLUMN felhasznalo.email IS 'A felhasználó email címe (jelszóval együtt egyedi)';
+COMMENT ON COLUMN felhasznalo.jelszo IS 'A felhasználó jelszava titkosítva (emaillel együtt egyedi)';
+COMMENT ON COLUMN felhasznalo.admin IS 'Igaz, ha a felhasználó adminisztrátor';
+
+CREATE TABLE feltolto (
+  felhasznalo_id NUMBER(10) NOT NULL,
+  video_id NUMBER(10) NOT NULL,
+  datum DATE NOT NULL,
+  PRIMARY KEY (felhasznalo_id, video_id)
+);
+
+COMMENT ON TABLE feltolto IS 'A videók feltöltését leíró tábla.';
+
+COMMENT ON COLUMN feltolto.felhasznalo_id IS 'A feltöltő felhasználó azonosítója (kulcs)';
+COMMENT ON COLUMN feltolto.video_id IS 'A videó azonosítója (kulcs)';
+COMMENT ON COLUMN feltolto.datum IS 'A videó feltöltésének dátuma';
+
+CREATE INDEX video_id ON feltolto (video_id);
+CREATE INDEX felhasznalo_id ON feltolto (felhasznalo_id);
+
+CREATE TABLE iro (
+  felhasznalo_id NUMBER(10) NOT NULL,
+  komment_id NUMBER(10) NOT NULL,
+  ido TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (felhasznalo_id, komment_id)
+);
+
+COMMENT ON TABLE iro IS 'A kommentek kiírását leíró tábla.';
+
+COMMENT ON COLUMN iro.felhasznalo_id IS 'A feltöltő felhasználó azonosítója (kulcs)';
+COMMENT ON COLUMN iro.komment_id IS 'A komment azonosítója (kulcs)';
+COMMENT ON COLUMN iro.ido IS 'A komment kiírásának dátuma és időpontja';
+
+CREATE INDEX felhasznalo_id ON iro (felhasznalo_id, komment_id);
+CREATE INDEX komment_id ON iro (komment_id);
+
+CREATE TABLE kategoria (
+  id NUMBER(10) NOT NULL,
+  cim VARCHAR2(60) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT cim_unique UNIQUE (cim)
+);
+
+COMMENT ON TABLE kategoria IS 'A videók lehetséges kategóriáit tartalmazó tábla.';
+
+CREATE SEQUENCE kategoria_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER kategoria_seq_tr
+BEFORE INSERT ON kategoria
+FOR EACH ROW
+WHEN (NEW.id IS NULL)
+BEGIN
+  SELECT kategoria_seq.NEXTVAL INTO :NEW.id FROM DUAL;
+END;
+/
+
+COMMENT ON COLUMN kategoria.id IS 'A kategória azonosítója (kulcs)';
+COMMENT ON COLUMN kategoria.cim IS 'A kategória címe (egyedi)';
+
+CREATE TABLE kedvenc (
+  felhasznalo_id NUMBER(10) NOT NULL,
+  video_id NUMBER(10) NOT NULL,
+  PRIMARY KEY (felhasznalo_id, video_id)
+);
+
+COMMENT ON TABLE kedvenc IS 'A felhasználók kedvelt videóit jelölő tábla.';
+
+COMMENT ON COLUMN kedvenc.felhasznalo_id IS 'A felhasználó azonosítója (kulcs)';
+COMMENT ON COLUMN kedvenc.video_id IS 'A kedvelt videó azonosítója (kulcs)';
+
+CREATE INDEX felhasznalo_id ON kedvenc (felhasznalo_id, video_id);
+CREATE INDEX video_id ON kedvenc (video_id);
+
+CREATE TABLE komment (
+  id NUMBER(10) NOT NULL,
+  szoveg VARCHAR2(1000) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+COMMENT ON TABLE komment IS 'A kommentek szövegét tartalmazó tábla.';
+
+CREATE SEQUENCE komment_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER komment_seq_tr
+ BEFORE INSERT ON komment FOR EACH ROW
+ WHEN (NEW.id IS NULL)
+BEGIN
+ SELECT komment_seq.NEXTVAL INTO :NEW.id FROM DUAL;
+END;
+/
+
+COMMENT ON COLUMN komment.id IS 'A komment azonosítója (kulcs)';
+COMMENT ON COLUMN komment.szoveg IS 'A komment szövege';
+
+CREATE TABLE video (
+  id number(10) NOT NULL,
+  cim varchar2(255 char) NOT NULL,
+  leiras varchar2(1000 char) NOT NULL,
+  path varchar2(255 char) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT path UNIQUE  (path)
+)  ;
+
+COMMENT ON TABLE video IS 'A videók adatait tartalmazó tábla.';
+
+CREATE SEQUENCE video_seq START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER video_seq_tr
+ BEFORE INSERT ON video FOR EACH ROW
+ WHEN (NEW.id IS NULL)
+BEGIN
+ SELECT video_seq.NEXTVAL INTO :NEW.id FROM DUAL;
+END;
+/
+
+COMMENT ON COLUMN video.id IS 'A videó azonosítója (kulcs)';
+COMMENT ON COLUMN video.cim IS 'A videó címe';
+COMMENT ON COLUMN video.leiras IS 'A videó leírása';
+COMMENT ON COLUMN video.path IS 'A videó elérési útvonala (egyedi)';
+
+CREATE TABLE video_cimke (
+  video_id number(10) NOT NULL,
+  cimke_id number(10) NOT NULL,
+  PRIMARY KEY (video_id,cimke_id)
+)  ;
+
+COMMENT ON TABLE video_cimke IS 'A videók egy-egy címkéjét jelölő tábla.';
+
+COMMENT ON COLUMN video_cimke.video_id IS 'A videó azonosítója (kulcs)';
+COMMENT ON COLUMN video_cimke.cimke_id IS 'Egy címke azonosítója (kulcs)';
+
+CREATE INDEX video_id ON video_cimke (video_id,cimke_id);
+CREATE INDEX cimke_id ON video_cimke (cimke_id);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE video_kategoria (
+  video_id number(10) NOT NULL,
+  kategoria_id number(10) NOT NULL,
+  PRIMARY KEY (video_id,kategoria_id)
+)  ;
 
---
--- Database: `videomegoszto`
---
-CREATE DATABASE IF NOT EXISTS `videomegoszto` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
-USE `videomegoszto`;
+COMMENT ON TABLE video_kategoria IS 'A videók kategoriáját jelölő tábla.';
 
--- --------------------------------------------------------
+COMMENT ON COLUMN video_kategoria.video_id IS 'A videó azonosítója (kulcs)';
+COMMENT ON COLUMN video_kategoria.kategoria_id IS 'A kategória azonosítója (kulcs)';
 
---
--- Table structure for table `cimke`
---
+CREATE INDEX video_id ON video_kategoria (video_id,kategoria_id);
+CREATE INDEX kategoria_id ON video_kategoria (kategoria_id);
 
-CREATE TABLE IF NOT EXISTS `cimke` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'A címke azonosítója (kulcs)',
-  `cim` int NOT NULL COMMENT 'A címke címe (egyedi)',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `cim` (`cim`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A videók lehetséges címkéit tartalmazó tábla.';
+ALTER TABLE eredet
+  ADD CONSTRAINT eredet_ibfk_1 FOREIGN KEY (komment_id) REFERENCES komment (id) ON DELETE CASCADE;
+ALTER TABLE eredet
+  ADD CONSTRAINT eredet_ibfk_2 FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE;
 
--- --------------------------------------------------------
+ALTER TABLE feltolto
+  ADD CONSTRAINT feltolto_ibfk_1 FOREIGN KEY (felhasznalo_id) REFERENCES felhasznalo (id) ON DELETE CASCADE;
+ALTER TABLE feltolto
+  ADD CONSTRAINT feltolto_ibfk_2 FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE;
 
---
--- Table structure for table `eredet`
---
+ALTER TABLE iro
+  ADD CONSTRAINT iro_ibfk_1 FOREIGN KEY (felhasznalo_id) REFERENCES felhasznalo (id) ON DELETE CASCADE;
+ALTER TABLE iro
+  ADD CONSTRAINT iro_ibfk_2 FOREIGN KEY (komment_id) REFERENCES komment (id) ON DELETE CASCADE;
 
-CREATE TABLE IF NOT EXISTS `eredet` (
-  `komment_id` int NOT NULL COMMENT 'A komment azonosítója (kulcs)',
-  `video_id` int NOT NULL COMMENT 'A videó azonosítója (kulcs)',
-  PRIMARY KEY (`komment_id`,`video_id`),
-  KEY `komment_id` (`komment_id`,`video_id`),
-  KEY `video_id` (`video_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A kommentek eredet videóját jelölő tábla.';
+ALTER TABLE kedvenc
+  ADD CONSTRAINT kedvenc_ibfk_1 FOREIGN KEY (felhasznalo_id) REFERENCES felhasznalo (id) ON DELETE CASCADE;
+ALTER TABLE kedvenc
+  ADD CONSTRAINT kedvenc_ibfk_2 FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE;
 
--- --------------------------------------------------------
+ALTER TABLE video_cimke
+  ADD CONSTRAINT video_cimke_ibfk_1 FOREIGN KEY (cimke_id) REFERENCES cimke (id) ON DELETE CASCADE;
+ALTER TABLE video_cimke
+  ADD CONSTRAINT video_cimke_ibfk_2 FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE;
 
---
--- Table structure for table `felhasznalo`
---
-
-CREATE TABLE IF NOT EXISTS `felhasznalo` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'A felhasználó azonosítója (kulcs)',
-  `nev` varchar(30) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'A felhasználónév',
-  `email` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'A felhaszánló email címe (jelszóval együtt\r\negyedi)',
-  `jelszo` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'A felhasználó jelszava titkosítva (emaillel\r\negyütt egyedi)',
-  `admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Igaz, ha a felhasználó adminisztrátor',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_jelszo` (`email`,`jelszo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A felhasználók adatait tartalmazó tábla.';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `feltolto`
---
-
-CREATE TABLE IF NOT EXISTS `feltolto` (
-  `felhasznalo_id` int NOT NULL COMMENT 'A feltöltő felhasználó azonosítója (kulcs)',
-  `video_id` int NOT NULL COMMENT 'A videó azonosítója (kulcs)',
-  `datum` date NOT NULL COMMENT 'A videó feltöltésének dátuma',
-  PRIMARY KEY (`felhasznalo_id`,`video_id`),
-  KEY `video_id` (`video_id`),
-  KEY `felhasznalo_id` (`felhasznalo_id`,`video_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A videók feltöltését leíró tábla.';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `iro`
---
-
-CREATE TABLE IF NOT EXISTS `iro` (
-  `felhasznalo_id` int NOT NULL COMMENT 'A feltöltő felhasználó azonosítója (kulcs)',
-  `komment_id` int NOT NULL COMMENT 'A komment azonosítója (kulcs)',
-  `ido` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'A komment kiírásának dátuma és időpontja',
-  PRIMARY KEY (`felhasznalo_id`,`komment_id`),
-  KEY `felhasznalo_id` (`felhasznalo_id`,`komment_id`),
-  KEY `komment_id` (`komment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A kommentek kiírását leíró tábla.';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `kategoria`
---
-
-CREATE TABLE IF NOT EXISTS `kategoria` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'A kategória azonosítója (kulcs)',
-  `cim` varchar(60) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'A kategória címe (egyedi)',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `cim` (`cim`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A videók lehetséges kategóriáit tartalmazó tábla.';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `kedvenc`
---
-
-CREATE TABLE IF NOT EXISTS `kedvenc` (
-  `felhasznalo_id` int NOT NULL COMMENT 'A felhasználó azonosítója (kulcs)',
-  `video_id` int NOT NULL COMMENT 'A kedvelt videó azonosítója (kulcs)',
-  PRIMARY KEY (`felhasznalo_id`,`video_id`),
-  KEY `felhasznalo_id` (`felhasznalo_id`,`video_id`),
-  KEY `video_id` (`video_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A felhasználók kedvelt videóit jelölő tábla.';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `komment`
---
-
-CREATE TABLE IF NOT EXISTS `komment` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'A komment azonosítója (kulcs)',
-  `szoveg` varchar(1000) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'A komment szövege',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A kommentek szövegét tartalmazó tábla.';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `video`
---
-
-CREATE TABLE IF NOT EXISTS `video` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'A videó azonosítója (kulcs)',
-  `cim` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'A videó címe',
-  `leiras` varchar(1000) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'A videó leírása',
-  `path` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'A videó elérési útvonala (egyedi)',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `path` (`path`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A videók adatait tartalmazó tábla.';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `video_cimke`
---
-
-CREATE TABLE IF NOT EXISTS `video_cimke` (
-  `video_id` int NOT NULL COMMENT 'A videó azonosítója (kulcs)',
-  `cimke_id` int NOT NULL COMMENT 'Egy címke azonosítója (kulcs)',
-  PRIMARY KEY (`video_id`,`cimke_id`),
-  KEY `video_id` (`video_id`,`cimke_id`),
-  KEY `cimke_id` (`cimke_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A videók egy-egy címkéjét jelölő tábla.';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `video_kategoria`
---
-
-CREATE TABLE IF NOT EXISTS `video_kategoria` (
-  `video_id` int NOT NULL COMMENT 'A videó azonosítója (kulcs)',
-  `kategoria_id` int NOT NULL COMMENT 'A kategória azonosítója (kulcs)',
-  PRIMARY KEY (`video_id`,`kategoria_id`),
-  KEY `video_id` (`video_id`,`kategoria_id`),
-  KEY `kategoria_id` (`kategoria_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci COMMENT='A videók kategoriáját jelölő tábla.';
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `eredet`
---
-ALTER TABLE `eredet`
-  ADD CONSTRAINT `eredet_ibfk_1` FOREIGN KEY (`komment_id`) REFERENCES `komment` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `eredet_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `feltolto`
---
-ALTER TABLE `feltolto`
-  ADD CONSTRAINT `feltolto_ibfk_1` FOREIGN KEY (`felhasznalo_id`) REFERENCES `felhasznalo` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `feltolto_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `iro`
---
-ALTER TABLE `iro`
-  ADD CONSTRAINT `iro_ibfk_1` FOREIGN KEY (`felhasznalo_id`) REFERENCES `felhasznalo` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `iro_ibfk_2` FOREIGN KEY (`komment_id`) REFERENCES `komment` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `kedvenc`
---
-ALTER TABLE `kedvenc`
-  ADD CONSTRAINT `kedvenc_ibfk_1` FOREIGN KEY (`felhasznalo_id`) REFERENCES `felhasznalo` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `kedvenc_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `video_cimke`
---
-ALTER TABLE `video_cimke`
-  ADD CONSTRAINT `video_cimke_ibfk_1` FOREIGN KEY (`cimke_id`) REFERENCES `cimke` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `video_cimke_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `video_kategoria`
---
-ALTER TABLE `video_kategoria`
-  ADD CONSTRAINT `video_kategoria_ibfk_1` FOREIGN KEY (`kategoria_id`) REFERENCES `kategoria` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `video_kategoria_ibfk_2` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE video_kategoria
+  ADD CONSTRAINT video_kategoria_ibfk_1 FOREIGN KEY (kategoria_id) REFERENCES kategoria (id) ON DELETE CASCADE;
+ALTER TABLE video_kategoria
+  ADD CONSTRAINT video_kategoria_ibfk_2 FOREIGN KEY (video_id) REFERENCES video (id) ON DELETE CASCADE;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
