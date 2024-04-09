297a258a013fbe26121c1f8fc9e9d4b4358ef170
Makó Ádám Péter <dumka1111@gmail.com>
2024-04-09 20:40:07 +0200

    thumbnail keszites feltoltesnel

--- src/php/upload.php
+++ src/php/upload.php
@@ -1,6 +1,7 @@
 <?php
 
 require "oracle_conn.php";
+require $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php';
 
 session_start();
 
@@ -79,6 +80,14 @@ else if (($_FILES["file"]["size"] > 104857600) || !in_array($extension, $allowed
 
     move_uploaded_file($_FILES["file"]["tmp_name"],
     $_SERVER["DOCUMENT_ROOT"] . "/media/videos/" . $vfname);
+
+    $sec = 10;
+    $movie = $_SERVER["DOCUMENT_ROOT"] . "/media/videos/" . $vfname;
+
+    $ffmpeg = FFMpeg\FFMpeg::create();
+    $video = $ffmpeg->open($movie);
+    $frame = $video->frame(FFMpeg\Coordinate\TimeCode::fromSeconds($sec));
+    $frame->save($_SERVER["DOCUMENT_ROOT"] . "/media/thumbnails/" . $tfname);
     
     $_SESSION['success'] = TRUE;
 }
