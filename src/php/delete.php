494a100303a22f5b4afe8e6f873a79343082d7ac
Makó Ádám Péter <dumka1111@gmail.com>
2024-04-16 11:14:16 +0200

    video torlesnel kommentek torlese

--- src/php/delete.php
+++ src/php/delete.php
@@ -7,6 +7,14 @@ session_start();
 $id = $_POST['video_id'];
 
 if (isset($_POST['video_torles'])) {
+  $delete_komment = oci_parse($conn, "
+    DELETE FROM KOMMENT
+    WHERE ID = ANY(
+      SELECT KOMMENT_ID
+      FROM EREDET
+      WHERE VIDEO_ID = :id)");
+  oci_bind_by_name($delete_komment, ":id", $id);
+
   $delete_feltolto = oci_parse($conn, "DELETE FROM feltolto WHERE video_id = :id");
   oci_bind_by_name($delete_feltolto, ':id', $id);
 
@@ -22,7 +30,7 @@ if (isset($_POST['video_torles'])) {
   $delete_video = oci_parse($conn, "DELETE FROM video WHERE id = :id");
   oci_bind_by_name($delete_video, ':id', $id);
 
-  if (oci_execute($delete_feltolto) && oci_execute($delete_eredet) && oci_execute($delete_kedvenc) && oci_execute($delete_video)) {
+  if (oci_execute($delete_komment) && oci_execute($delete_feltolto) && oci_execute($delete_eredet) && oci_execute($delete_kedvenc) && oci_execute($delete_video)) {
     $_SESSION['delete_success'] = $id . " ID-vel rendelkező videó sikeresen törölve lett.";
     header("Location: ./../delete_page.php");
   } else {
