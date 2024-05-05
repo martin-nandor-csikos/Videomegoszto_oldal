3888966aef06f11c1b15f44864d12a95e99b980c
Martin Nándor Csíkos <csikos.martin17@gmail.com>
2024-05-05 23:34:05 +0200

    last minute valtoztatasok

--- src/menu.php
+++ src/menu.php
@@ -6,13 +6,14 @@ echo "<a href=\"index.php\">Főoldal</a><br />";
 if (isset($_SESSION['user_id'])) {
     echo "<p>Üdvözöljük, " . $_SESSION['user_name'] . "!</p>";
     echo "<a href=\"user_data.php\">Fiókom adatai</a><br />";
-    if ($_SESSION['user_isadmin'] == 0) {
-        echo "<a href=\"videos_by_current_user.php\">Feltöltött videóim</a><br />";
-        echo "<a href=\"upload_page.php\">Videó feltöltés</a><br />";
-        echo "<a href=\"favorite_videos_page.php\">Kedvenc videók</a><br />";
-    } else {
+    
+    if ($_SESSION['user_isadmin'] == 1) {
         echo "<a href=\"delete_page.php\">Adatok törlése és módosítása</a><br />";
     }
+
+    echo "<a href=\"videos_by_current_user.php\">Feltöltött videóim</a><br />";
+    echo "<a href=\"upload_page.php\">Videó feltöltés</a><br />";
+    echo "<a href=\"favorite_videos_page.php\">Kedvenc videók</a><br />";
     echo "<a href=\"php/logout.php\">Kijelentkezés</a>";
 } else {
     echo "<a href=\"login_page.php\">Bejelentkezés</a>";
